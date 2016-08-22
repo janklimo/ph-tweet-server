5202308f498252688e98d215bc6bec34cddfba6f
Jan Klimo <jan.klimo@gmail.com>
2016-08-22 18:43:40 +0700

    add signature, expose hunters

--- app/serializers/entry_serializer.rb
+++ app/serializers/entry_serializer.rb
@@ -1,8 +1,12 @@
 class EntrySerializer < ActiveModel::Serializer
-  attributes :id, :date, :makers
+  attributes :id, :date, :makers, :hunters
   has_many :posts
 
   def makers
     object.users.maker.where.not(twitter: nil).map { |m| m.twitter }
+  end
+
+  def hunters
+    object.users.hunter.where.not(twitter: nil).map { |m| m.twitter }
   end
 end
